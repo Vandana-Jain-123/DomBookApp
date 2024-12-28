82332a3dfcbb7caa26a59ae90d61219079a12e27
Vandana-jain-123 <vandana12345jain@gmail.com>
2024-12-28 15:52:37 +0530

    login part and half admin part completed

---
+++ script/footer.js
@@ -0,0 +1,10 @@
+const footer=()=>{
+let card=`
+<div id="">
+<h5>This is footer</h5>
+</div>
+`
+document.getElementById("footer").innerHTML=card
+
+}
+footer()
---
+++ script/navbar.js
@@ -0,0 +1,14 @@
+
+const  navbar=()=>{
+let card=`
+<div id="nav-container">
+<a href="index.html">Home</a>
+<a href="admin.html">Admin</a>
+<a href="books.html">Books</a>
+
+ 
+`
+document.getElementById("nav").innerHTML=card
+
+}
+navbar()
